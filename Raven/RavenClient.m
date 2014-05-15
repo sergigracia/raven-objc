b3a7f9cc47e73fa20879fc5c072d33c17e536a16
Sergi Gracia <sergigram@gmail.com>
2014-05-15 13:07:59 +0200

    Adding support to remove memory adresses and group better the messages in Sentry

--- Raven/RavenClient.m
+++ Raven/RavenClient.m
@@ -46,13 +46,9 @@ void exceptionHandler(NSException *exception) {
 }
 
 - (void)setTags:(NSDictionary *)tags {
-    [self setTags:tags withDefaultValues:YES];
-}
-
-- (void)setTags:(NSDictionary *)tags withDefaultValues:(BOOL)withDefaultValues {
     NSMutableDictionary *mTags = [[NSMutableDictionary alloc] initWithDictionary:tags];
 
-    if (withDefaultValues && ![mTags objectForKey:@"Build version"]) {
+    if (![mTags objectForKey:@"Build version"]) {
         NSString *buildVersion = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"];
         if (buildVersion) {
             [mTags setObject:buildVersion forKey:@"Build version"];
@@ -60,12 +56,12 @@ void exceptionHandler(NSException *exception) {
     }
 
 #if TARGET_OS_IPHONE
-    if (withDefaultValues && ![mTags objectForKey:@"OS version"]) {
+    if (![mTags objectForKey:@"OS version"]) {
         NSString *osVersion = [[UIDevice currentDevice] systemVersion];
         [mTags setObject:osVersion forKey:@"OS version"];
     }
 
-    if (withDefaultValues && ![mTags objectForKey:@"Device model"]) {
+    if (![mTags objectForKey:@"Device model"]) {
         struct utsname systemInfo;
         uname(&systemInfo);
         NSString *deviceModel = [NSString stringWithCString:systemInfo.machine
@@ -136,21 +132,6 @@ void exceptionHandler(NSException *exception) {
 }
 
 - (void)captureMessage:(NSString *)message level:(RavenLogLevel)level method:(const char *)method file:(const char *)file line:(NSInteger)line {
-
-    [self captureMessage:message level:level additionalExtra:nil additionalTags:nil method:method file:file line:line];
-}
-
-- (void)captureMessage:(NSString *)message level:(RavenLogLevel)level additionalExtra:(NSDictionary *)additionalExtra additionalTags:(NSDictionary *)additionalTags {
-    [self captureMessage:message level:level additionalExtra:additionalExtra additionalTags:additionalTags method:nil file:nil line:0];
-}
-
-- (void)captureMessage:(NSString *)message
-                 level:(RavenLogLevel)level
-       additionalExtra:(NSDictionary *)additionalExtra
-        additionalTags:(NSDictionary *)additionalTags
-                method:(const char *)method
-                  file:(const char *)file
-                  line:(NSInteger)line {
     NSArray *stacktrace;
     if (method && file && line) {
         NSDictionary *frame = [NSDictionary dictionaryWithObjectsAndKeys:
@@ -164,8 +145,6 @@ void exceptionHandler(NSException *exception) {
 
     NSDictionary *data = [self prepareDictionaryForMessage:message
                                                      level:level
-                                           additionalExtra:additionalExtra
-                                            additionalTags:additionalTags
                                                    culprit:file ? [NSString stringWithUTF8String:file] : nil
                                                 stacktrace:stacktrace
                                                  exception:nil];
@@ -180,15 +159,11 @@ void exceptionHandler(NSException *exception) {
 }
 
 - (void)captureException:(NSException *)exception sendNow:(BOOL)sendNow {
-   [self captureException:exception additionalExtra:nil additionalTags:nil sendNow:sendNow];
-}
-
-- (void)captureException:(NSException *)exception additionalExtra:(NSDictionary *)additionalExtra additionalTags:(NSDictionary *)additionalTags sendNow:(BOOL)sendNow {
     NSString *message = [NSString stringWithFormat:@"%@: %@", exception.name, exception.reason];
-
+    
     NSDictionary *exceptionDict = [NSDictionary dictionaryWithObjectsAndKeys:
                                    exception.name, @"type",
-                                   exception.reason, @"value",
+                                   [self hideMemoryAdressesofString:exception.reason], @"value",
                                    nil];
 
     NSArray *callStack = [exception callStackSymbols];
@@ -199,8 +174,6 @@ void exceptionHandler(NSException *exception) {
 
     NSDictionary *data = [self prepareDictionaryForMessage:message
                                                      level:kRavenLogLevelDebugFatal
-                                           additionalExtra:additionalExtra
-                                            additionalTags:additionalTags
                                                    culprit:nil
                                                 stacktrace:stacktrace
                                                  exception:exceptionDict];
@@ -221,6 +194,18 @@ void exceptionHandler(NSException *exception) {
     } else {
         [self sendDictionary:data];
     }
+}
+
+- (NSString *)hideMemoryAdressesofString:(NSString *)message
+{
+    NSError *error;
+    NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"0x[a-z0-9]*" options:NSRegularExpressionCaseInsensitive error:&error];
+    
+    if (error) {
+        NSLog(@"Error trying to remove memory adress from Raven Message: %@", error.description);
+    }
+    
+    return [regex stringByReplacingMatchesInString:message options:nil range:NSMakeRange(0, [message length]) withTemplate:@"0x*********"];
 }
 
 - (void)setupExceptionHandler {
@@ -250,22 +235,10 @@ void exceptionHandler(NSException *exception) {
 
 - (NSDictionary *)prepareDictionaryForMessage:(NSString *)message
                                         level:(RavenLogLevel)level
-                              additionalExtra:(NSDictionary *)additionalExtra
-                               additionalTags:(NSDictionary *)additionalTags
                                       culprit:(NSString *)culprit
                                    stacktrace:(NSArray *)stacktrace
                                     exception:(NSDictionary *)exceptionDict {
     NSDictionary *stacktraceDict = [NSDictionary dictionaryWithObjectsAndKeys:stacktrace, @"frames", nil];
-
-    NSMutableDictionary *extra = [NSMutableDictionary dictionaryWithDictionary:self.extra];
-    if (additionalExtra.count) {
-        [extra addEntriesFromDictionary:additionalExtra];
-    }
-
-    NSMutableDictionary *tags = [NSMutableDictionary dictionaryWithDictionary:self.tags];
-    if (additionalTags.count) {
-        [tags addEntriesFromDictionary:additionalTags];
-    }
 
     return [NSDictionary dictionaryWithObjectsAndKeys:
             [self generateUUID], @"event_id",
@@ -274,8 +247,8 @@ void exceptionHandler(NSException *exception) {
             kRavenLogLevelArray[level], @"level",
             @"objc", @"platform",
 
-            extra, @"extra",
-            tags, @"tags",
+            self.extra, @"extra",
+            self.tags, @"tags",
 
             message, @"message",
             culprit ?: @"", @"culprit",
